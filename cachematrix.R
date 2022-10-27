2dbe645be9f9f3d91f56466fb59b260742c94fa1
Amritesh0107 <116835971+Amritesh0107@users.noreply.github.com>
2022-10-27 23:23:51 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First in the function makeCacheMatrix, why does 'set function' use variable 'y'?
+Is it simply used to tell us how to use '<<-'?
 
-## Write a short comment describing this function
+#Secondly, In 'get function', why is variable 'x' followed by the parentheses of the function? ( 'setInverse function - inv<<-inverse' , 'getInverse function - inv' are the same. )
+
+
+##x <<- y creates x, which is then retrieved by get(). Note that get is also a base function and is being overwritten here. I would advocate in avoiding this, even though it's confined to a function. An ad hoc solution would be to reach base get function through base::get.
+
+#Line get <- function() x is a "short" version of
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+            x <<- y
+            inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
